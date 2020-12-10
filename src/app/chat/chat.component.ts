81eae1413902f0eccaaa5d10c1b3946badfaef16
Alisson Diane <aarteaga@tabernero.com>
2020-12-10 00:14:23 -0500

    Chat component

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Client} from '@stomp/stompjs';
 import * as SockJS from 'sockjs-client';
+import {Mensaje} from './models/mensaje';
 
 @Component({
   selector: 'app-chat',
@@ -10,6 +11,11 @@ import * as SockJS from 'sockjs-client';
 export class ChatComponent implements OnInit {
   private client: Client;
 
+  conectado: boolean = false;
+
+  mensaje: Mensaje = new Mensaje();
+
+  mensajes: Mensaje[] = [];
 
   constructor() { }
 
@@ -21,9 +27,38 @@ export class ChatComponent implements OnInit {
 
     this.client.onConnect = (frame) => {
       console.log('Conectados:' + this.client.connected + ' : ' + frame);
-    }
-    this.client.activate();
+      this.conectado = true;
+      this.client.subscribe('/chat/mensaje', e => {
+        let mensaje: Mensaje = JSON.parse(e.body) as Mensaje;
+        mensaje.fecha = new Date(mensaje.fecha);
+        this.mensajes.push(mensaje);
+        console.log(mensaje);
+      });
+      this.mensaje.tipo = 'NUEVO USUARIO';
 
+      this.client.publish({destination: '/app/mensaje', body: JSON.stringify(this.mensaje)});
+
+    }
+
+    this.client.onDisconnect = (frame)=>{
+      console.log('Desconectados:' + !this.client.connected + ' : ' + frame);
+      this.conectado = false;
+    }
+    
+
+  }
+
+  conectar():void{
+    this.client.activate();
+  }
+
+  desconectar():void{
+    this.client.deactivate();
+  }
+  enviarMensaje():void{
+    this.mensaje.tipo = 'MENSAJE';
+    this.client.publish({destination: '/app/mensaje', body: JSON.stringify(this.mensaje)});
+    this.mensaje.texto = '';
   }
 
 }
